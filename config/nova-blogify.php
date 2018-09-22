6ae46ee78709c31541b7bfe6fdd792e183214e17
G. Arends <gd@wmc.io>
2018-09-22 14:27:11 -0400

    Add collection to config file

--- config/nova-blogify.php
+++ config/nova-blogify.php
@@ -28,6 +28,7 @@ return [
     'image_settings' => [
         'disk' => env('BLOGIFY_DISK_NAME', 'public'),
         'path' => env('BLOGIFY_IMAGE_PATH', ''),
+        'collection' => env('BLOGIFY_IMAGE_COLLECTION', 'posts'),
         'path_thumb' => env('BLOGIFY_THUM_PATH', 'thumb/'),
     ],
 
